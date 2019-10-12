9b4ae89bc522f9580a340ec1d1e0aa7419610fbd
vishneuski <vishneuski87@gmail.com>
2019-10-12 09:25:08 +0300

    work with contact component form

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,6 +1,5 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {RegisterInfo} from "../register/model/register-info";
 
 @Component({
   selector: 'app-contacts',
@@ -14,37 +13,15 @@ export class ContactsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
-
-}
-
-export class RegisterViewComponent implements OnInit {
-  registerForm: FormGroup;
-
-  @Input()
-  user: RegisterInfo;
-
-  @Output()
-  registerTry: EventEmitter<RegisterInfo> = new EventEmitter<RegisterInfo>();
-
-  constructor() { }
-
-  ngOnInit() {
-
-    this.registerForm = new FormGroup({
+    this.contactMessageForm = new FormGroup({
       'firstName': new FormControl(null, Validators.required),
-      'lastName': new FormControl(null, Validators.required),
-      'dateOfBirth': new FormControl(null, Validators.required),
-      'phone': new FormControl(null, Validators.required),
       'email': new FormControl(null, [Validators.required, Validators.email]),
-      'password': new FormControl(null, Validators.required),
-      'repeatPassword': new FormControl(null, Validators.required),
+      'message': new FormControl(null, Validators.required),
     })
   }
 
   onSubmit(): void {
-    console.log(this.registerForm.value);
-    this.registerTry.emit(this.registerForm.value);
+    console.log(this.contactMessageForm.value);
   }
 
 }
